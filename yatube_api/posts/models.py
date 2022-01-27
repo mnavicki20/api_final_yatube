23a1486f13846c4a14f47fb4dac57d3b986e71a9
Mikita Navicki <bulbanik91@gmail.com>
2022-01-27 20:53:01 +0300

    Создана модель Follow

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -1,3 +1,4 @@
+from tkinter import CASCADE
 from django.contrib.auth import get_user_model
 from django.db import models
 
@@ -24,3 +25,10 @@ class Comment(models.Model):
     text = models.TextField()
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='follower')
+    following = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='following')
