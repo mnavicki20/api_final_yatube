a340eb142a1c33e1fc4f5f9596b48550cb883884
Mikita Navicki <bulbanik91@gmail.com>
2022-01-30 14:53:31 +0300

    Для FollowViewSet созданы и использованы миксины

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,5 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, viewsets
+from rest_framework import filters, viewsets, mixins
 from rest_framework.generics import get_object_or_404
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import (IsAuthenticated,
@@ -9,6 +9,11 @@ from .permissions import IsOwnerOrReadOnly
 from .serializers import (CommentSerializer, GroupSerializer,
                           PostSerializer, FollowSerializer)
 from posts.models import Group, Post
+
+
+class BaseViewSet(mixins.CreateModelMixin, mixins.ListModelMixin,
+                  viewsets.GenericViewSet):
+    pass
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -45,7 +50,7 @@ class CommentViewSet(viewsets.ModelViewSet):
         return post.comments.all()
 
 
-class FollowViewSet(viewsets.ModelViewSet):
+class FollowViewSet(BaseViewSet):
     """Вьюсет для подписок."""
     serializer_class = FollowSerializer
     permission_classes = [IsAuthenticated]
